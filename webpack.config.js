5add0d82db72cda127e55080129efdfe0a0eaca2
Sayful Islam <sayful.islam001@gmail.com>
2020-11-28 17:55:51 +0530

    Move elements directory items to components directory.

--- webpack.config.js
+++ webpack.config.js
@@ -5,31 +5,19 @@ const TerserPlugin = require('terser-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 const combineMediaQuery = require('postcss-combine-media-query');
 const svgToMiniDataURI = require('mini-svg-data-uri');
+const WebpackCleanPlugin = require('webpack-clean');
 
 let plugins = [];
 let entryPoints = {
   shapla: "./src/shapla.scss",
-  elements: "./src/elements/_all.scss",
   components: "./src/components/_all.scss",
-  "color-theme": "./src/color-theme.scss",
-  flexbox: "./src/flexbox.scss",
-  grid: "./src/grid.scss",
-  sizing: "./src/sizing/_all.scss",
-  typography: "./src/typography/_all.scss",
-  spacing: [
-    "./src/spacing/_margin.scss",
-    "./src/spacing/_padding.scss",
-    "./src/spacing/_space-between.scss",
-  ],
-  "responsive-spacing": [
-    "./src/spacing/_margin-responsive.scss",
-    "./src/spacing/_padding-responsive.scss",
-  ],
 };
 
 plugins.push(new MiniCssExtractPlugin({
   filename: "./[name].css"
 }));
+
+plugins.push(new WebpackCleanPlugin(['dist/components.js', 'dist/shapla.js']));
 
 module.exports = (env, argv) => {
   let isDev = argv.mode !== 'production';
